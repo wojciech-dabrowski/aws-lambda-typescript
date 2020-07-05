b837d65908b85600ecda2b5f7833cd2ab5b2b6da
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2020-07-05 13:48:54 +0200

    Change project structure and used libraries, cleanup

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 npm run build
 sam deploy `
-    --template-file .aws-sam/build/template.yaml `
+    --template-file template.yaml `
     --stack-name my-stack-name `
     --s3-bucket my-serverless-deploy-bucket `
     --s3-prefix my-stack-prefix/ `
