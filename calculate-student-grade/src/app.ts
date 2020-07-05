b3cf2eb2960246386a337a15652f5e7765e8e1c2
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2020-07-05 14:09:59 +0200

    Implement calculating final grade

--- calculate-student-grade/src/app.ts
+++ calculate-student-grade/src/app.ts
@@ -1,9 +1,8 @@
-import { CalculateStudentGradeHandler } from "./calculate-student-grade-handler";
+import { calculateStudentGradeHandler } from "./calculate-student-grade-handler";
 import { CalculateStudentGradeResponse } from "./models/calculate-student-grade-response";
 import { CalculateStudentGradeRequest } from "./models/calculate-student-grade-request";
 
-const functionHandler: CalculateStudentGradeHandler = new CalculateStudentGradeHandler();
-
 export const handler = async (
   request: CalculateStudentGradeRequest
-): Promise<CalculateStudentGradeResponse> => functionHandler.handle(request);
+): Promise<CalculateStudentGradeResponse> =>
+  calculateStudentGradeHandler(request);
--- calculate-student-grade/src/calculate-student-grade-handler.ts
+++ calculate-student-grade/src/calculate-student-grade-handler.ts
@@ -1,16 +1,38 @@
 import { CalculateStudentGradeRequest } from "./models/calculate-student-grade-request";
 import { CalculateStudentGradeResponse } from "./models/calculate-student-grade-response";
 
-export class CalculateStudentGradeHandler {
-  public handle = async (
-    request: CalculateStudentGradeRequest
-  ): Promise<CalculateStudentGradeResponse> => {
-    console.log("Hello from function handler!");
-    console.log(`Payload: ${JSON.stringify(request)}`);
+export const calculateStudentGradeHandler = async (
+  request: CalculateStudentGradeRequest
+): Promise<CalculateStudentGradeResponse> => {
+  const finalGrade = calculateFinalGrade(
+    request.lectureGrade,
+    request.exerciseGrade,
+    request.workshopGrade
+  );
 
-    return {
-      finalGrade: 5,
-      hasPassed: true,
-    };
+  return {
+    finalGrade: finalGrade,
+    hasPassed: finalGrade >= 3,
   };
-}
+};
+
+const calculateFinalGrade = (
+  lectureGrade: number,
+  exerciseGrade: number,
+  workshopGrade: number
+): number => {
+  const lectureWeight = 0.2;
+  const exerciseWeight = 0.3;
+  const workshopWeight = 0.5;
+
+  const rawFinalGrade =
+    lectureGrade * lectureWeight +
+    exerciseGrade * exerciseWeight +
+    workshopGrade * workshopWeight;
+
+  return roundToNearestHalf(rawFinalGrade);
+};
+
+const roundToNearestHalf = (value: number): number => {
+  return Math.round(value * 2) / 2;
+};
